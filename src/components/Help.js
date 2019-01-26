abed96c0ccb43fbf81418630c39b45d9ae82f946
Jonathan Striebel <jonathan.striebel@gmail.com>
2019-01-26 15:52:24 +0100

    prettier README & code

--- src/components/Help.js
+++ src/components/Help.js
@@ -22,16 +22,15 @@ class Help extends React.Component {
               Du kannst hier eigene Kreuzworträtsel erstellen, inspiriert vom{" "}
               <a href="https://sz-magazin.sueddeutsche.de/tag/kreuzwortraetsel">
                 Kreuz mit den Worten im Süddeutschen Zeitung Magazin
-              </a>,
-              die unter dem Pseudonym{" "}
+              </a>
+              , die unter dem Pseudonym{" "}
               <a href="https://de.wikipedia.org/wiki/CUS_(R%C3%A4tselautor)">
                 CUS
               </a>{" "}
               wöchentlich erscheinen.
             </p>
             <p>
-              Die meisten Inhalte können durch anklicken editiert
-              werden:
+              Die meisten Inhalte können durch anklicken editiert werden:
               <ul>
                 <li>Anfang des Magazintitels</li>
                 <li>Untertitel (links)</li>
@@ -40,25 +39,29 @@ class Help extends React.Component {
                 <li>AutorInnenkürzel (genau 3 Buchstaben)</li>
                 <li>Fragen</li>
               </ul>
-              Um Wortgrenzen hinzuzufügen musst du das Buchstabenfeld nach dem Wort
-              markieren und Enter drücken. Dabei bitte auf die richtige
+              Um Wortgrenzen hinzuzufügen musst du das Buchstabenfeld nach dem
+              Wort markieren und Enter drücken. Dabei bitte auf die richtige
               Schreibrichtung achten.
             </p>
             <p>
-              Das fertige Kreuzworträtsel kann ausgedruckt werden, die Lösungen und
-              Buttons werden dabei nicht mitgedruckt.
+              Das fertige Kreuzworträtsel kann ausgedruckt werden, die Lösungen
+              und Buttons werden dabei nicht mitgedruckt.
             </p>
           </Tabs.TabPane>
           <Tabs.TabPane tab="Über" key="Über">
             KreuzWort ist
             <ul>
               <li>
-                eine <a href="https://jonathanstriebel.de/kreuzwort">Homepage</a>{" "}
-                um selbst Kreuzworträtsel zu erstellen,</li>
+                eine{" "}
+                <a href="https://jonathanstriebel.de/kreuzwort">Homepage</a> um
+                selbst Kreuzworträtsel zu erstellen,
+              </li>
               <li>
-                dem <a href="https://sz-magazin.sueddeutsche.de/tag/kreuzwortraetsel">
+                dem{" "}
+                <a href="https://sz-magazin.sueddeutsche.de/tag/kreuzwortraetsel">
                   Kreuz mit den Worten
-                </a> des Süddeutschen Zeitung Magazins entlehnt,
+                </a>{" "}
+                des Süddeutschen Zeitung Magazins entlehnt,
               </li>
               <li>
                 programmiert von{" "}
@@ -69,7 +72,11 @@ class Help extends React.Component {
                 <a href="https://de.wikipedia.org/wiki/MIT-Lizenz">
                   MIT-Lizenz
                 </a>{" "}
-                auf <a href="https://github.com/jstriebel/kreuzwort">GitHub veröffentlicht</a>.
+                auf{" "}
+                <a href="https://github.com/jstriebel/kreuzwort">
+                  GitHub veröffentlicht
+                </a>
+                .
               </li>
             </ul>
           </Tabs.TabPane>
